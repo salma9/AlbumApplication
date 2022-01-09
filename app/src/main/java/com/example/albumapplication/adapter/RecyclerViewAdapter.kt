e63537e4747b80c69faa66e1085ae30df0abbd0e
makni salma <makni.salma9@gmail.com>
2022-01-09 01:26:00 +0100

    display album list

---
+++ app/src/main/java/com/example/albumapplication/adapter/RecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.albumapplication.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.albumapplication.domain.models.Album
+import com.example.albumapplication.R
+import com.squareup.picasso.Picasso
+
+class RecyclerViewAdapter(private val albumList: List<Album>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: RecyclerViewAdapter.ViewHolder, position: Int) {
+        holder.bind(albumList[position])
+    }
+    override fun getItemCount(): Int {
+        return albumList.size
+    }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(album: Album) {
+            val txtAlbumTitle = itemView.findViewById<TextView>(R.id.title)
+            val imgAlbum = itemView.findViewById<ImageView>(R.id.image)
+
+            txtAlbumTitle.text = album.title
+
+            //load album's image
+            Picasso.get()
+                .load(album.thumbnailUrl)
+                .placeholder(R.drawable.ic_baseline_image_not_supported_24)
+                .error(R.drawable.ic_baseline_image_not_supported_24)
+                .into(imgAlbum)
+        }
+    }
+}
--- app/src/main/java/com/example/albumapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/albumapplication/view/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.albumapplication.view
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.albumapplication.domain.utils.Constants
+import com.example.albumapplication.adapter.RecyclerViewAdapter
 import com.example.albumapplication.databinding.ActivityMainBinding
 import com.example.albumapplication.di.DaggerAppComponent
 import com.example.albumapplication.viewModel.MainViewModel
@@ -14,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     @Inject
     lateinit var viewModel: MainViewModel
+    private lateinit var mAdapter: RecyclerViewAdapter
     private val TAG: String = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +28,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Layout manager
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        viewModel.albumList.observe(this,{
+            it?.let {
+                mAdapter = RecyclerViewAdapter(it)
+                binding.recyclerView.adapter = mAdapter
+                setVisibilities(View.INVISIBLE, View.VISIBLE)
+            }?: run{
+                setVisibilities(View.VISIBLE, View.INVISIBLE)
+            }
+        })
+
         viewModel.getAllAlbums()
+    }
+
+    /**
+     * set view's visibility
+     */
+    fun setVisibilities(textVisibility: Int, recyclerVisibility: Int){
+        binding.recyclerView.visibility = recyclerVisibility
+        binding.textView.visibility = textVisibility
     }
 }
